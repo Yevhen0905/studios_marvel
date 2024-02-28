53ad8dee9b2675e050c1e348894b01ecad9293bd
Vittcenko <evgenav83@gmail.com>
2024-02-28 19:32:54 +0200

    fix style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,17 +11,5 @@
 </script>
 
 <style lang="scss">
-  .logo_marvel {
-    position: absolute;
-    top: 0;
-    left: 0;
-    max-width: 150px;
-    opacity: 0.7;
-    box-shadow: 0px 13px 8px #d42417;
-  }
-  @media screen and (max-width: 768px) {
-    .logo_marvel {
-      max-width: 100px;
-    }
-  }
+
 </style>
